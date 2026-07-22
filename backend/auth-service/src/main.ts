f5f8408f06ebfa1638143edf8dcc63c002a77c16
kan-ckm <at2356116@gmail.com>
2026-07-22 23:08:41 +0700

    feat: thêm các file .env.exxample và chuẩn bị setup prisma

--- backend/auth-service/src/main.ts
+++ backend/auth-service/src/main.ts
@@ -1,16 +1,19 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	//khai báo cổng máy chủ Grpc và thiết lập sử dụng đúng bản hợp đồng auth.proto
+	const config = app.get(ConfigService)
+	const port_grpc = config.getOrThrow<string>('AUTH_GRPC_URL')
+	//khai báo cổng máy chủ Grpc auth và thiết lập sử dụng đúng bản hợp đồng auth.proto
 	app.connectMicroservice<MicroserviceOptions>({
 		transport: Transport.GRPC,
 		options: {
 			package: 'auth.v1',
 			protoPath: 'node_modules/@tomatocinema/contracts/proto/auth.proto',
-			url: 'localhost:50051',
+			url: port_grpc,
 			loader: {
 				keepCase: false,
 				longs: String,
