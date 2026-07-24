da1d0c1dbab08ffe399f594da6a30d6fef6b2aef
kan-ckm <at2356116@gmail.com>
2026-07-24 23:03:26 +0700

    feat:bắt đầu viết logic gửi mã otp

--- backend/auth-service/src/module/auth/auth.repository.ts
+++ backend/auth-service/src/module/auth/auth.repository.ts
@@ -1,11 +1,28 @@
-import { PrismaService } from '@/infrastucture/prisma/prisma.service';
 import { Injectable } from '@nestjs/common'
+import { Account } from 'generated/client'
+import { PrismaService } from '@/infrastucture/prisma/prisma.service'
+import { AccountCreateInput } from 'generated/models'
 
 @Injectable()
 export class AuthRepository {
-    public constructor (private readonly prismaService: PrismaService) {
+	public constructor(private readonly prismaService: PrismaService) {}
 
+	public async findByPhone(phone: string): Promise<Account | null> {
+		return await this.prismaService.account.findUnique({
+			where: {
+				phone
+			}
+		})
+	}
+	public async findByEmail(email: string): Promise<Account | null> {
+		return await this.prismaService.account.findUnique({
+			where: {
+				email
+			}
+		})
+	}
+
+    public async create(data: AccountCreateInput) {
+        
     }
-
-    
 }
--- backend/auth-service/src/module/auth/auth.service.ts
+++ backend/auth-service/src/module/auth/auth.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@nestjs/common'
 import { SendOtpRequest } from '@tomatocinema/contracts/gen/auth'
+import { Account } from 'generated/client'
+import { AuthRepository } from './auth.repository'
 
 @Injectable()
 export class AuthService {
-	public async sendOtp(data: SendOtpRequest) {}
+	public constructor(private readonly authRepository: AuthRepository) {}
+
+	public async sendOtp(data: SendOtpRequest) {
+		try {
+			const { identifier, type } = data
+			let account: Account | null
+
+			if (type === 'phone') {
+				account = await this.authRepository.findByPhone(identifier)
+			} else {
+				account = await this.authRepository.findByEmail(identifier)
+			}
+			if
+		} catch (error) {
+			throw new Error("Lỗi auth gửi otp", error);
+		}
+	}
 }
