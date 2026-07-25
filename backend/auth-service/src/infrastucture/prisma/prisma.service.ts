6bb9f54992c27ca9d5a0874b63018e14c9a1b319
kan-ckm <at2356116@gmail.com>
2026-07-25 14:46:10 +0700

    style:sửa lại lỗi chính tả

--- backend/auth-service/src/infrastucture/prisma/prisma.service.ts
+++ backend/auth-service/src/infrastucture/prisma/prisma.service.ts
@@ -20,7 +20,7 @@ export class PrismaService
 			user: configService.getOrThrow<string>('POSTGRES_USER'),
 			password: configService.getOrThrow<string>('POSTGRES_PASSOWRD'),
 			host: configService.getOrThrow<string>('POSTGRES_HOST'),
-			port: configService.getOrThrow<number>('POSTGRE_PORT'),
+			port: configService.getOrThrow<number>('POSTGRES_PORT'),
 			database: configService.getOrThrow<string>('POSTGRES_DATABASE')
 		})
 
