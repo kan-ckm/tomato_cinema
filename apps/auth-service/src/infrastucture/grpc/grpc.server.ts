b5cde3b26ea1df8c59b13c5ef41f98339e14bcf0
kan-ckm <at2356116@gmail.com>
2026-08-01 21:06:59 +0700

    refactor:đổi và chia nhỏ ra nhiệm vụ khai báo máy chủ grpc cho auth

---
+++ apps/auth-service/src/infrastucture/grpc/grpc.server.ts
@@ -0,0 +1,24 @@
+import { INestApplication } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { AllConfig } from 'config/interfaces'
+import { grpcLoader, grpcPackages, grpcProtoPaths } from './grpc-options'
+
+export function createGrpcServer(
+	app: INestApplication,
+	config: ConfigService<AllConfig>
+) {
+	const host = config.get('grpc.host', { infer: true })
+	const port = config.get('grpc.port', { infer: true })
+	const url = `${host}:${port}`
+	//khai báo cổng máy chủ Grpc auth và thiết lập sử dụng đúng bản hợp đồng auth.proto
+	app.connectMicroservice<MicroserviceOptions>({
+		transport: Transport.GRPC,
+		options: {
+			package: grpcPackages,
+			protoPath: grpcProtoPaths,
+			url,
+			loader: grpcLoader
+		}
+	})
+}
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -7,26 +7,8 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	const config = app.get(ConfigService<AllConfig>)
-	const host = config.get('grpc.host', { infer: true })
-	const port = config.get('grpc.port', { infer: true })
-	const url = `${host}:${port}`
-	//khai báo cổng máy chủ Grpc auth và thiết lập sử dụng đúng bản hợp đồng auth.proto
-	app.connectMicroservice<MicroserviceOptions>({
-		transport: Transport.GRPC,
-		options: {
-			package: 'auth.v1',
-			protoPath: PROTO_PATHS.AUTH,
-			url: url,
-			loader: {
-				keepCase: false,
-				longs: String,
-				enums: String,
-				default: true,
-				oneofs: true
-			}
-		}
-	})
+	const config = app.get(ConfigService)
+
 	await app.startAllMicroservices()
 	await app.init()
 }
