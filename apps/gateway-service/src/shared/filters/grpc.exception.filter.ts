e8c1eadf844a377310e3210c3a1bb1644cbb13ab
kan-ckm <at2356116@gmail.com>
2026-08-02 22:26:56 +0700

    docs: add thêm comment giải thích hàm GrcpExceotionFilter

--- apps/gateway-service/src/shared/filters/grpc.exception.filter.ts
+++ apps/gateway-service/src/shared/filters/grpc.exception.filter.ts
@@ -8,6 +8,7 @@ import {
 import { Response } from 'express'
 import { grpcToHttpStatus } from '../utils'
 
+// hàm giúp cho việc tự gom lỗi r tự gửi lại cho frontend mã lỗi của http hay vì mã Grpc
 @Catch()
 export class GrcpExceotionFilter implements ExceptionFilter {
 	public catch(exception: any, host: ArgumentsHost) {
