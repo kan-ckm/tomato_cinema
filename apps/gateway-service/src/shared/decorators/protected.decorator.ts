59347f2c9bd111e16460aea9037f7e79c12a1169
kan-ckm <at2356116@gmail.com>
2026-08-05 10:32:35 +0700

    docs: thêm comment giải thích  một xíu

--- apps/gateway-service/src/shared/decorators/protected.decorator.ts
+++ apps/gateway-service/src/shared/decorators/protected.decorator.ts
@@ -1,4 +1,6 @@
 import { applyDecorators, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '../guards'
 
+// Decorator bảo vệ api
+
 export const Protected = () => applyDecorators(UseGuards(AuthGuard))
