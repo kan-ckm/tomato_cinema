038050ed36fd4426be79eb5902137e9cdc8bc7d4
kan-ckm <at2356116@gmail.com>
2026-08-08 17:13:41 +0700

    feat: setup env bot telegram

---
+++ apps/auth-service/config/env/telegram-env.ts
@@ -0,0 +1,15 @@
+import { registerAs } from '@nestjs/config'
+import { TelegramConfig } from 'config/interfaces'
+import { TelegramValidator } from 'config/validators'
+import { validateEnv } from '@/shared/utils/env'
+
+export const telegramEnv = registerAs<TelegramConfig>('telegram', () => {
+	validateEnv(process.env, TelegramValidator)
+
+	return {
+		botId: process.env.TELEGRAM_BOT_ID,
+		botToken: process.env.TELEGRAM_BOT_TOKEN,
+		botUsername: process.env.TELEGRAM_BOT_USERNAME,
+		redirectOrigin: process.env.TELEGRAM_REDIRECT_ORIGIN
+	}
+})
--- apps/auth-service/config/interfaces/all-config-intertface.ts
+++ apps/auth-service/config/interfaces/all-config-intertface.ts
@@ -2,10 +2,12 @@ import { DatabaseConfig } from './database-interface'
 import { GrpcConfig } from './grpc-interface'
 import { PassportConfig } from './passport-interface'
 import { RedisConfig } from './redis-interface'
+import { TelegramConfig } from './telegram-interface'
 
 export interface AllConfig {
 	grpc: GrpcConfig
 	database: DatabaseConfig
 	redis: RedisConfig
 	passport: PassportConfig
+	telegram: TelegramConfig
 }
